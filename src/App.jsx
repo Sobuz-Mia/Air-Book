c0eb3379f3682ebc469078888d9320f16f497885
Sobuz <sobuz.info0298@gmail.com>
2024-03-19 16:50:07 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Banner from './pages/home/Banner/Banner'
+import Navbar from './pages/home/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar/>
+     <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            This is our navbar
+        </div>
+    );
+};
+
+export default Navbar;
